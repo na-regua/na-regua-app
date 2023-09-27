8a4c1d9920f73cf5641282e5f05b52e010baee85
Alex Rodrigues <alex.rodrigues@venturus.org.br>
2023-09-27 13:13:49 -0300

    feat: renaming app ios and android adjust

--- ios/NaReguaTests/NaReguaTests.m
+++ ios/NaReguaTests/NaReguaTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface appTests : XCTestCase
+@interface NaReguaTests : XCTestCase
 
 @end
 
-@implementation appTests
+@implementation NaReguaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
